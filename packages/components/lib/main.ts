40e81540061a44c6fa9e3044ce666134e19786e1
Andreas Riedmüller <andreas@simplease.at>
2024-05-23 12:09:53 +0200

    export hooks

--- packages/components/lib/main.ts
+++ packages/components/lib/main.ts
@@ -1,6 +1,20 @@
 export { Button, ButtonA } from "./Button";
 export { FormField } from "./FormField";
 export { InlineIcon } from "./InlineIcon";
-export { TextInput } from "./TextInput";
 export { Label } from "./Label";
 export { Stack } from "./Stack";
+export { TextInput } from "./TextInput";
+
+export { useButton } from "./Button/useButton";
+export { useFormField } from "./FormField/useFormField";
+export { useInlineIcon } from "./InlineIcon/useInlineIcon";
+export { useLabel } from "./Label/useLabel";
+export { useStack } from "./Stack/useStack";
+export { useTextInput } from "./TextInput/useTextInput";
+
+export { useBaseButton } from "./Button/useBaseButton";
+export { useBaseFormField } from "./FormField/useBaseFormField";
+export { useBaseInlineIcon } from "./InlineIcon/useBaseInlineIcon";
+export { useBaseLabel } from "./Label/useBaseLabel";
+export { useBaseStack } from "./Stack/useBaseStack";
+export { useBaseTextInput } from "./TextInput/useBaseTextInput";
