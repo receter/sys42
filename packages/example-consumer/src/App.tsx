de4b9f8042917e5fedae2ae06ca3440b8956d26d
Andreas Riedmüller <andreas@simplease.at>
2024-07-11 11:28:26 +0200

    simplify example-consumer App

--- packages/example-consumer/src/App.tsx
+++ packages/example-consumer/src/App.tsx
@@ -1,6 +1,5 @@
 import "@sys42/components/default-custom-properties.css";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button, classCard, classInlineIcon } from "@sys42/components";
@@ -10,26 +9,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className={classCard}>Hello I am a card!</div>
-      <div className="card">
-        <Button onPress={() => setCount((count) => count + 1)}>
-          <img src={viteLogo} className={classInlineIcon} /> count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <div className={classCard}>Hello I am a sys42 card!</div>
+
+      <Button
+        aria-busy="true"
+        className="example-button"
+        onPress={() => setCount((count) => count + 1)}
+      >
+        <img src={viteLogo} className={classInlineIcon} /> count is {count}
+      </Button>
+
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
       </p>
     </>
   );
