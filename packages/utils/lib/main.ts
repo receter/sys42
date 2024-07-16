7c9263e0355ab75b1f6e5d144b3bc455e4b4df33
Andreas Riedmüller <andreas@simplease.at>
2024-07-16 17:09:44 +0200

    add functions to utils and link in components, bump versions

--- packages/utils/lib/main.ts
+++ packages/utils/lib/main.ts
@@ -1,4 +1,33 @@
-export function concatClassNames(...args: Array<string | boolean | null | undefined>)
-  : string {
-  return args.filter(item => !!item).join(' ');
+export function concatClassNames(
+  ...args: Array<string | boolean | null | undefined>
+): string {
+  return args.filter((item) => !!item).join(" ");
+}
+
+export function filterKeyEnter(handler: (e: React.KeyboardEvent) => void) {
+  return (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      handler(e);
+    }
+  };
+}
+
+export function filterKeyEsc(handler: (e: React.KeyboardEvent) => void) {
+  return (e: React.KeyboardEvent) => {
+    if (e.key === "Escape") {
+      handler(e);
+    }
+  };
+}
+
+export function accessibleOnClick(
+  handler: (e: React.KeyboardEvent) => void,
+  tabIndex: number
+) {
+  return {
+    role: "button",
+    tabIndex: tabIndex || 0,
+    onKeyDown: filterKeyEnter(handler),
+    onClick: handler,
+  };
 }
