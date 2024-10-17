d6fee97ad0c3787691e97dfa1b28504e50d61c74
Andreas Riedmüller <andreas@simplease.at>
2024-10-17 15:16:38 +0200

    docs: add TSDoc for util functions

--- packages/utils/lib/main.ts
+++ packages/utils/lib/main.ts
@@ -1,11 +1,27 @@
+/**
+ * Concatenates class names, filtering out any falsey values.
+ * You can use "cn" as an alias for this function.
+ *
+ * @param args - The class names to concatenate.
+ * @returns The concatenated class names.
+ */
 export function concatClassNames(
   ...args: Array<string | boolean | null | undefined>
 ): string {
   return args.filter((item) => !!item).join(" ");
 }
 
+/**
+ * Alias for `concatClassNames`.
+ */
 export const cn = concatClassNames;
 
+/**
+ * Filters keyboard events to only trigger the handler when the Enter key is pressed.
+ *
+ * @param handler - The event handler to call when the Enter key is pressed.
+ * @returns A function that filters keyboard events.
+ */
 export function filterKeyEnter(handler: (e: React.KeyboardEvent) => void) {
   return (e: React.KeyboardEvent) => {
     if (e.key === "Enter") {
@@ -14,6 +30,12 @@ export function filterKeyEnter(handler: (e: React.KeyboardEvent) => void) {
   };
 }
 
+/**
+ * Filters keyboard events to trigger the handler when either the Enter or Space key is pressed.
+ *
+ * @param handler - The event handler to call when the Enter or Space key is pressed.
+ * @returns A function that filters keyboard events.
+ */
 export function filterKeyTriggerButton(
   handler: (e: React.KeyboardEvent) => void
 ) {
@@ -24,6 +46,12 @@ export function filterKeyTriggerButton(
   };
 }
 
+/**
+ * Filters keyboard events to only trigger the handler when the Escape key is pressed.
+ *
+ * @param handler - The event handler to call when the Escape key is pressed.
+ * @returns A function that filters keyboard events.
+ */
 export function filterKeyEsc(handler: (e: React.KeyboardEvent) => void) {
   return (e: React.KeyboardEvent) => {
     if (e.key === "Escape") {
@@ -32,6 +60,13 @@ export function filterKeyEsc(handler: (e: React.KeyboardEvent) => void) {
   };
 }
 
+/**
+ * Provides accessibility attributes and event handlers for an JSX HTML element to be used as a button.
+ *
+ * @param handler - The event handler to call on click or keydown Enter/Space key.
+ * @param tabIndex - The tab index for the element.
+ * @returns An object containing accessibility attributes and event handlers to be spread on a JSX HTML element.
+ */
 export function accessibleOnClick(
   handler: (e: React.KeyboardEvent | React.MouseEvent) => void,
   tabIndex?: number
